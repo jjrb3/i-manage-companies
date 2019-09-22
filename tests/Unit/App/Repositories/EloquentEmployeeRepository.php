e20656cdbc0625f353495b8673fd9ef61bfaab32
Jeremy Reyes B <jjrb6@hotmail.com>
2019-09-22 14:21:53 -0500

    [test]: Unit test repository.

--- tests/Unit/App/Repositories/EloquentEmployeeRepository.php
+++ tests/Unit/App/Repositories/EloquentEmployeeRepository.php
@@ -2,17 +2,17 @@
 
 namespace Tests\Unit\App\Repositories;
 
-
 use App\Models\Company;
+use App\Models\Employee;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
 /**
- * Class EloquentCompanyRepository
+ * Class EloquentEmployeeRepository
  * @package Tests\Unit\App\Repositories
  */
-class EloquentCompanyRepository extends TestCase
+class EloquentEmployeeRepository extends TestCase
 {
     use DatabaseTransactions, WithoutMiddleware;
 
@@ -33,26 +33,29 @@ class EloquentCompanyRepository extends TestCase
     {
         parent::setUp();
 
-        $this->repository = $this->app->make('App\Repositories\EloquentCompanyRepository');
+        $this->repository = $this->app->make('App\Repositories\EloquentEmployeeRepository');
         $this->parameters = [
-            'name' => 'Company test',
+            'first_name' => 'Company test',
+            'last_name' => '',
             'email' => '',
-            'logo' => '',
-            'website' => ''
+            'phone' => ''
         ];
-
     }
 
     /**
-     * Fin all companies
+     * Fin all employees
      *
      * @return void
      */
     public function testFindAllPaginate()
     {
-        factory(Company::class, 30)->create();
+        $company = factory(Company::class)->create();
 
-        $result = $this->repository->findAllPaginate();
+        factory(Employee::class, 30)->create([
+            'company_id' => $company->id
+        ]);
+
+        $result = $this->repository->findAllPaginateByCompany($company->id);
 
         $this->assertEquals($result->count(), 10);
         $this->assertEquals($result->toArray()['total'], 30);
@@ -62,44 +65,50 @@ class EloquentCompanyRepository extends TestCase
     }
 
     /**
-     * Create company.
+     * Create employee.
      *
      * @return void
      */
     public function testCreate()
     {
+        $this->parameters['company_id'] = factory(Company::class)->create()->id;
+
         $result = $this->repository->create($this->parameters);
 
-        $this->assertEquals($result->name, $this->parameters['name']);
+        $this->assertEquals($result->first_name, $this->parameters['first_name']);
+        $this->assertEquals($result->last_name, $this->parameters['last_name']);
         $this->assertEquals($result->email, $this->parameters['email']);
-        $this->assertEquals($result->logo, $this->parameters['logo']);
-        $this->assertEquals($result->website, $this->parameters['website']);
+        $this->assertEquals($result->phone, $this->parameters['phone']);
     }
 
     /**
-     * Update company
+     * Update employee
      *
      * @return void
      */
     public function testUpdate()
     {
-        $companies = factory(Company::class)->create();
+        $employee = factory(Employee::class)->create([
+            'company_id' => factory(Company::class)->create()->id
+        ]);
 
-        $result = $this->repository->updateById($companies->id, $this->parameters);
+        $result = $this->repository->updateById($employee->id, $this->parameters);
 
         $this->assertTrue($result);
     }
 
     /**
-     * Destroy company
+     * Destroy employee
      *
      * @return void
      */
     public function testDestroy()
     {
-        $companies = factory(Company::class)->create();
+        $employee = factory(Employee::class)->create([
+            'company_id' => factory(Company::class)->create()->id
+        ]);
 
-        $result = $this->repository->destroyById($companies->id);
+        $result = $this->repository->destroyById($employee->id);
 
         $this->assertTrue($result);
     }
