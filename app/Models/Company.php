97a99bbce7ef0587fb698ec01bae12b9d88f20b6
Jeremy Reyes B <jjrb6@hotmail.com>
2019-09-22 12:21:04 -0500

    [config]: Added models.

---
+++ app/Models/Company.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Company
+ * @package App
+ */
+class Company extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = "companies";
+}
---
+++ app/Models/Employee.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Employee
+ * @package App
+ */
+class Employee extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = "employees";
+}
