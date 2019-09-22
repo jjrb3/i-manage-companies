b0ababb94d816ccf4510ebd4a171e6e7daea9e2d
Jeremy Reyes B <jjrb6@hotmail.com>
2019-09-22 10:44:05 -0500

    [feat]: Added Ample admin theme to laravel mix.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+// Ample admin theme
+mix.copy('resources/themes/ample-admin-lite-master/plugins','public/themes/ample-admin-lite-master/plugins');
+mix.copy('resources/themes/ample-admin-lite-master/html/bootstrap','public/themes/ample-admin-lite-master/bootstrap');
+mix.copy('resources/themes/ample-admin-lite-master/html/js','public/themes/ample-admin-lite-master/js');
+mix.copy('resources/themes/ample-admin-lite-master/html/css','public/themes/ample-admin-lite-master/css');
+mix.copy('resources/themes/ample-admin-lite-master/html/less','public/themes/ample-admin-lite-master/less');
